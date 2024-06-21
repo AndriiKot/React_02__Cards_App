2c2f66ad64ef8d8282e880920ca16bd5bdb24a9c
AndriiKot <a_kocyuba@ukr.net>
2024-06-21 12:33:34 +0200

    React 'Only React_02' ! TESTING component 'Video' testing 1  !

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,13 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
+import Video from "./Video";
 
 function App() {
   const number = "02";
   return (
     <>
       <div className="video-container">
-        <div className="video">
-          <img className="video-image" src={reactLogo} alt="Video image" />
-          <p>Video 1</p>
-          <p>React JS</p>
-          <div className="video-footer">
-            <p>Linkes: 0</p>
-            <button>like</button>
-          </div>
-        </div>
-        <div className="video">
-          <img className="video-image" src={reactLogo} alt="Video image" />
-          <p>Video 1</p>
-          <p>React JS</p>
-          <div className="video-footer">
-            <p>Linkes: 0</p>
-            <button>like</button>
-          </div>
-        </div>
-        <div className="video">
-          <img className="video-image" src={reactLogo} alt="Video image" />
-          <p>Video 1</p>
-          <p>React JS</p>
-          <div className="video-footer">
-            <p>Linkes: 0</p>
-            <button>like</button>
-          </div>
-        </div>
-        <div className="video">
-          <img className="video-image" src={reactLogo} alt="Video image" />
-          <p>Video 1</p>
-          <p>React JS</p>
-          <div className="video-footer">
-            <p>Linkes: 0</p>
-            <button>like</button>
-          </div>
-        </div>
-        <div className="video">
-          <img className="video-image" src={reactLogo} alt="Video image" />
-          <p>Video 1</p>
-          <p>React JS</p>
-          <div className="video-footer">
-            <p>Linkes: 0</p>
-            <button>like</button>
-          </div>
-        </div>
-        <div className="video">
-          <img className="video-image" src={reactLogo} alt="Video image" />
-          <p>Video 1</p>
-          <p>React JS</p>
-          <div className="video-footer">
-            <p>Linkes: 0</p>
-            <button>like</button>
-          </div>
-        </div>
+        <Video />
       </div>
     </>
   );
---
+++ src/Video.jsx
@@ -0,0 +1,19 @@
+﻿import reactLogo from "./assets/react.svg";
+
+function Video() {
+  return (
+    <>
+      <div className="video">
+        <img className="video-image" src={reactLogo} alt="Video image" />
+        <p>Video 1</p>
+        <p>React JS</p>
+        <div className="video-footer">
+          <p>Linkes: 0</p>
+          <button>like</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Video;
