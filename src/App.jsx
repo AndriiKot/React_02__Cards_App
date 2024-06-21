c22ddba15b978b229d8eda234b9bd2f29e4f2031
AndriiKot <a_kocyuba@ukr.net>
2024-06-21 09:48:53 +0200

    React 'Only React_02' ! array tesing 1 !

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,14 @@ import "./App.css";
 
 function App() {
   const number = "02";
+  const name = [
+    "Andrii",
+    "Kot",
+    "https://github.com/AndriiKot",
+  ]
   return (
     <>
+    {name}
       <h1>
         Only React {number} <img src={reactLogo} alt="React" />
       </h1>
