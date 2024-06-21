571497e37d5e624fa22af2b83bf4405d438750c3
AndriiKot <a_kocyuba@ukr.net>
2024-06-21 11:58:29 +0200

    React 'Only React_02' ! added classes to App.jsx !

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,17 @@ function App() {
   const number = "02";
   return (
     <>
-    <div>
-      <div>
-        <img src={reactLogo} alt="Video image" />
-        <p>Video 1</p>
-        <p>React JS</p>
-        <div>
-          <p>Linkes: 0</p>
-          <button>like</button>
+      <div className="video-container">
+        <div className="video">
+          <img className="video-image" src={reactLogo} alt="Video image" />
+          <p>Video 1</p>
+          <p>React JS</p>
+          <div className="video-footer">
+            <p>Linkes: 0</p>
+            <button>like</button>
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
