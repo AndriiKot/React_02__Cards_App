cb1bea7a51eb00e9d767838f7f25c93f9af4bdb8
AndriiKot <a_kocyuba@ukr.net>
2024-06-21 09:59:14 +0200

    React 'Only React_02' ! <p> added !

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,10 @@ import "./App.css";
 function App() {
   const number = "02";
   return (
-    <>
       <h1>
         Only React {number} <img src={reactLogo} alt="React" />
       </h1>
-    </>
+      <p>Some text </p>
   );
 }
 
