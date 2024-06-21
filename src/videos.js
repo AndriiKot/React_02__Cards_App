d51a2ec0f54d116894204bcf66ff935819ad953c
AndriiKot <a_kocyuba@ukr.net>
2024-06-21 14:24:35 +0200

    React 'Only React_02' ! CREATE new file videos.js and create array 'VIDEOS' !

---
+++ src/videos.js
@@ -0,0 +1,47 @@
+﻿import reactLogo from "./assets/react.svg";
+
+export const VIDEOS = [
+  {
+    id: 2,
+    title: "Video 1",
+    channelName: "React JS",
+    img: reactLogo,
+  },
+  {
+    id: 2,
+    title: "Video 2",
+    channelName: "React JS",
+    img: reactLogo,
+  },
+  {
+    id: 3,
+    title: "Video 3",
+    channelName: "React JS",
+    img: reactLogo,
+  },
+  {
+    id: 4,
+    title: "Video 4",
+    channelName: "React JS",
+    img: reactLogo,
+  },
+  {
+    id: 5,
+    title: "Video 5",
+    channelName: "React JS",
+    img: reactLogo,
+  },
+  {
+    id: 6,
+    title: "Video 6",
+    channelName: "React JS",
+    img: reactLogo,
+  },
+
+  {
+    id: 7,
+    title: "Video 7",
+    channelName: "React JS",
+    img: reactLogo,
+  },
+];
