f29153dc28f8245ecef6f581d7862c1c2f666143
AndriiKot <a_kocyuba@ukr.net>
2024-06-21 09:56:00 +0200

    React 'Only React_02' ! comment tesing 1 !

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,11 @@ function App() {
   const number = "02";
   const name = [
     "Andrii",
-    "Kot",
-    "https://github.com/AndriiKot",
   ]
   return (
     <>
-    {name}
+    {name} {/* name.join('') */}
+    
       <h1>
         Only React {number} <img src={reactLogo} alt="React" />
       </h1>
