9c8fec95f8036377904e8f30a2d0f38fa2ea8dca
AndriiKot <a_kocyuba@ukr.net>
2024-06-30 14:01:27 +0200

    React 'Only React_02' ! CREATE NEW CARDS 'Cats' !

---
+++ src/Cats/images.js
@@ -0,0 +1,58 @@
+﻿
+export const IMAGES = [
+  {
+    id: 1,
+    title: "image 1",
+    alt: "sleeping cat on a blanket",
+    src: "https://cdn.freecodecamp.org/curriculum/css-photo-gallery/1.jpg",
+  },
+  {
+    id: 2,
+    title: "image 2",
+    alt: "kitten lying on its back on a wooden floor",
+    src: "https://cdn.freecodecamp.org/curriculum/css-photo-gallery/2.jpg",
+  },
+  {
+    id: 3,
+    title: "image 3",
+    alt: "The face of a serious cat",
+    src: "https://cdn.freecodecamp.org/curriculum/css-photo-gallery/3.jpg",
+  },
+  {
+    id: 4,
+    title: "image 4",
+    alt: "Sleeping cat under a blanket",
+    src: "https://cdn.freecodecamp.org/curriculum/css-photo-gallery/4.jpg",
+  },
+  {
+    id: 5,
+    title: "image 5",
+    alt: "surprised kitten near the window",
+    src: "https://cdn.freecodecamp.org/curriculum/css-photo-gallery/5.jpg",
+  },
+  {
+    id: 6,
+    title: "image 6",
+    alt: "two kittens twins",
+    src: "https://cdn.freecodecamp.org/curriculum/css-photo-gallery/6.jpg",
+  },
+
+  {
+    id: 7,
+    title: "image 7",
+    alt: "playful kitten looks out from under the blanket",
+    src: "https://cdn.freecodecamp.org/curriculum/css-photo-gallery/7.jpg",
+  },
+  {
+    id: 8,
+    title: "image 8",
+    alt: "red kitten on a chair",
+    src: "https://cdn.freecodecamp.org/curriculum/css-photo-gallery/8.jpg",
+  },
+  {
+    id: 9,
+    title: "image 9",
+    alt: "two tired tots lying down, one white the other black",
+    src: "https://cdn.freecodecamp.org/curriculum/css-photo-gallery/9.jpg",
+  },
+];
